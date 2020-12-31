6f872d4afd8de9e42c1cb8f8344d08045d294a9f
Bharti Swami <bhartiswamitech@gmail.com>
2021-01-01 01:51:01 +0530

    finding Fibonacci number at perticular index using recursion

---
+++ JAVALOVE/src/javalove/RecursionFibonacci.java
@@ -0,0 +1,18 @@
+package javalove;
+import java.util.*;
+
+public class RecursionFibonacci {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0) {
+			int n=sc.nextInt();
+			System.out.println(fibo(n));
+		}
+	}
+	public static int fibo(int n) {
+		if(n==0||n==1)
+		return n;
+		return fibo(n-1)+fibo(n-2);
+	}
+}
