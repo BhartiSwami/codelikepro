6a9ce9a5c47459a906377944ab1987f3ad142c53
Bharti Swami <bhartiswamitech@gmail.com>
2021-02-05 02:32:23 +0530

    Finding subsequences of the given string using recursion

--- JAVALOVE/src/PrintRecursion/Subsequences.java
+++ JAVALOVE/src/PrintRecursion/Subsequences.java
@@ -10,8 +10,8 @@ public class Subsequences {
 		}
 		char ch=ques.charAt(0);                                      //taking first character of the string
 		String roq=ques.substring(1);                              //rest of the string except first character
-		PrintAllSubsequences(roq,ans);                           //Prints all the subsequences where ch is not present
 		PrintAllSubsequences(roq,ans+ch);                       //Prints all the possible subsequences in which ch is present
+		PrintAllSubsequences(roq,ans);                           //Prints all the subsequences where ch is not present
 	}
 	
 	
